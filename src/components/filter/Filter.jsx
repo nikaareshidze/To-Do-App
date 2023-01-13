dc57d86d82df3866eddc79a16bbac42d2cc7afd5
nikaareshidze <nikaareshidzeapps@gmail.com>
2023-01-14 02:09:19 +0400

    Create FilterComponent for code improvement

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { toDoListActions } from "../../store/toDoListSlice";
+import { useSelector } from "react-redux";
 
 import FilterTitle from "./FilterTitle";
+import FilterComponent from "./FilterComponent";
 
 export default function Filter() {
-  const dispatch = useDispatch();
-  const setFilterRender = (payload) => {
-    dispatch(toDoListActions.setFilterRender(payload));
-  };
-
-  const { toDoList, filterRender } = useSelector(
-    (state) => state.toDoListSlice
-  );
+  const { filterRender } = useSelector((state) => state.toDoListSlice);
 
   return (
     <div className="flex gap-x-8">
@@ -22,37 +15,10 @@ export default function Filter() {
         <FilterTitle title="All" />
       )}
 
-      {toDoList.filter((x) => x.status == "In Progress").length == 0 ? (
-        <h1>In Progress</h1>
-      ) : filterRender == "In Progress" ? (
-        <FilterTitle title="In Progress" font="font-bold" />
-      ) : (
-        <FilterTitle title="In Progress" />
-      )}
-
-      {toDoList.filter((x) => x.status == "Plan").length == 0 ? (
-        <h1>Plan</h1>
-      ) : filterRender == "Plan" ? (
-        <FilterTitle title="Plan" font="font-bold" />
-      ) : (
-        <FilterTitle title="Plan" />
-      )}
-
-      {toDoList.filter((x) => x.status == "Done").length == 0 ? (
-        <h1>Done</h1>
-      ) : filterRender == "Done" ? (
-        <FilterTitle title="Done" font="font-bold" />
-      ) : (
-        <FilterTitle title="Done" />
-      )}
-
-      {toDoList.filter((x) => x.status == "Not Started").length == 0 ? (
-        <h1>Not Started</h1>
-      ) : filterRender == "Not Started" ? (
-        <FilterTitle title="Not Started" font="font-bold" />
-      ) : (
-        <FilterTitle title="Not Started" />
-      )}
+      <FilterComponent title="In Progress" />
+      <FilterComponent title="Plan" />
+      <FilterComponent title="Done" />
+      <FilterComponent title="Not Started" />
     </div>
   );
 }
---
+++ src/components/filter/FilterComponent.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import FilterTitle from "./FilterTitle";
+
+export default function FilterComponent({ title }) {
+  const { toDoList, filterRender } = useSelector(
+    (state) => state.toDoListSlice
+  );
+
+  return (
+    <>
+      {toDoList.filter((x) => x.status == title).length == 0 ? (
+        <h1>{title}</h1>
+      ) : filterRender == title ? (
+        <FilterTitle title={title} font="font-bold" />
+      ) : (
+        <FilterTitle title={title} />
+      )}
+    </>
+  );
+}
