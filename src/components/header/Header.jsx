65cfe4b6c7f548857de157a92c253002c8388490
nikaareshidze <nikaareshidzeapps@gmail.com>
2023-04-16 23:46:36 +0400

    remove search

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,9 +6,9 @@ export default function Header() {
   return (
     <div className="flex justify-between items-center mb-8">
       <Logo />
-      <div className="max-[750px]:hidden">
+      {/* <div className="max-[750px]:hidden">
         <Search />
-      </div>
+      </div> */}
       <Toolkit />
     </div>
   );
