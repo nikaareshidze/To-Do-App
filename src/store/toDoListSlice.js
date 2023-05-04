e9a29a9fbd819bb9b3da0bef8fef42e9c811d349
nikaareshidze <nikaareshidzeapps@gmail.com>
2023-05-05 01:26:25 +0400

    add initial list

--- src/store/toDoListSlice.js
+++ src/store/toDoListSlice.js
@@ -1,7 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const toDoListInitialState = {
-  toDoList: [],
+  toDoList: [
+    {
+      title:
+        "Clean out your closet and donate any clothes you no longer wear to a local charity.",
+      status: "Done",
+    },
+    {
+      title:
+        "Write a short story about a character who goes on an unexpected adventure.",
+      status: "In Progress",
+    },
+    {
+      title:
+        "Create a budget plan for the next month, including your income, expenses, and savings goals.",
+      status: "Not Started",
+    },
+    {
+      title: "Learn a new recipe and cook a meal from scratch.",
+      status: "Not Started",
+    },
+  ],
   id: 0,
   filterRender: "All",
 };
